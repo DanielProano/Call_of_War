0b89588110a173655b393cc73ed07200f6def6b6
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-07-13 12:28:11 -0400

    Updated Welcome with ASCII

--- welcome.py
+++ welcome.py
@@ -1,29 +1,54 @@
 def print_welcome():
-    print("=" * 70)
-    print("                  Welcome to the Call of War - Battle Simulator!")
-    print("=" * 70)
-    print()
-    print("Recreate and explore battle scenarios right from your terminal —")
-    print("no waiting, no hassle, just strategy and fun.")
-    print()
-    print("If you like what you see, please consider starring the repo!")
-    print("-" * 70)
-    print("Note: Currently, only Axis units are available.")
-    print("> > > For detailed instructions, check out tutorial.py < < <")
-    print("-" * 70)
-    print()
-    print("*" * 70)
-    print("This project was built solo by Daniel Proano, a Computer Science")
-    print("student at Purdue University.")
-    print()
-    print("If you enjoyed the project and want to support me, I'm")
-    print("actively seeking an internship opportunity!")
-    print()
-    print("LinkedIn : https://www.linkedin.com/in/daniel-proano-20976b32a/")
-    print("Email    : djz.labz@gmail.com")
-    print("*" * 70)
+	print("""
+  -...         .:=+==:.       :.                                      
+     :-.          .-=+-=::    @@:  ++:     %@=                        
+         .::             *...#@@##+##%---.=#@*           ..           
+                ..       * +***++%@@@#= #@#@@%*-::                  . 
+                 .=  :=. -+----::..... .=+****-=:+#=-                 
+                  .  .#@@@#*+#=+-==+*=+++===-==:==****#%%             
+                  ==+++++=-::::-:::--:----:::..::=@####%%     .       
+                 :-===::.:+*+*+-::===-::=::-=...:=##@@@%+==::==+=: :: 
+                 --...-**#%@@###########+#++++**++%@%*++=+-----------=
+        =@=*+**#%#*@@@@@@@@@@@@@@@@@@@@@@%**###*+==-::::-------=::-   
+             .:::-+#%@@@@@@@@@@@@@@@@@@%#*=:::::-::-::-:+=--==-=%@@  .
+      -+==+*+=---===+***********+++++=:::-::-----:-=:-=---------%*+- :
+    .:::::-++*##%+=-----=*++==+----=+++=---=--------::::---=**####+***
+    =@@@#+*%@%#*+++:-::-%@*+%*-..  .:==+=-----------+*%@@%%@%%@@*+==++
+      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*=+=-:-=+#@@@@#@@#@@%@@%%@@=  .::
+      %@@@@@@@@@@@@@@@@@@@@@@@@@@@%@%@@@@%@@%@@#@@#%@@%@@#@@#%@+      
+  .    #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+@@#@@##@%*@@*%%@#%@%##*+        
+        =@@@@@@@@@@@@@@%@@@@@@@@@@@@@@%%%@@#*@@*@@@#%#+               
+::+@%@@@@=*-*#***+:-:%#%#*:+@@@@@@@@@@@@*@@@##%##-                    
+@@@@@@##*==*-=-:::.:==##+++=-+=**%#*%-.  .                         .*#
+""")
+	print("=" * 70)
+	print("                  Welcome to the Call of War - Battle Simulator!")
+	print("=" * 70)
+	print()
+	print("Recreate and explore battle scenarios right from your terminal —")
+	print()
+	print("no waiting, no hassle, just strategy and fun.")
+	print()
+	print("If you like what you see, please consider starring the repo!")
+	print("\n" * 1)
+	print("-" * 70)
+	print("Note: Currently, only Axis units are available.")
+	print("> > > For detailed instructions, check out tutorial.py < < <")
+	print("-" * 70)
+	print("=" * 70)
+	print("\n" * 2)
+	print("*" * 70)
+	print("This was a solo project built by Daniel Proano, a Computer Science")
+	print("student at Purdue University.")
+	print()
+	print("If you enjoyed the project and want to support me, I'm")
+	print("actively seeking an internship opportunity!")
+	print()
+	print("LinkedIn : https://www.linkedin.com/in/daniel-proano-20976b32a/")
+	print("Email    : djz.labz@gmail.com")
+	print("*" * 70)
 
 if __name__ == "__main__":
-    print_welcome()
+	print_welcome()
 
 
